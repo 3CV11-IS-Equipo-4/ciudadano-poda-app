c415cb577c6d1ac466419663c78f22ae95b8ac1a
Poncho1409 <lhernandezm1409@alumno.ipn.mx>
2021-03-25 03:22:32 -0600

    Mixed views

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Layout.css'
-// import Navbar from './Navbar';
+import Navbar from './Navbar';
 
-function Layout({head, subheading,children}){
+function Layout({head, subheading,children,footer}){
     return(
         <div className="container-fluid text-center layout">
-        {/*Navbar*/}
-        <h1>{head}</h1>
-        <h2>{subheading}</h2>
-        <div className="container d-flex justify-content-center align-items-center flex-column">
-            {children}            
-        </div>
-
+            {Navbar}
+            <h1>{head}</h1>
+            <h2>{subheading}</h2>
+            <div className="container d-flex justify-content-center align-items-center flex-column">
+                {children}            
+            </div>
+            
         </div>
     );
 }
--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import Layout from '../Components/Layout';
+import Card from '../Components/Card';
+
 export default function Page404() {
     return(
         <>
             <Navbar type={"ciudadano"}></Navbar>
-
-            <h1>Esa ruta no existe</h1>
-            <Link to="/">Volver al inicio</Link>
+            <Layout head="Solicitud en vía pública" subheading="Datos del ciudadano" children={<div className="d-flex justify-content-between"><Card></Card><Card></Card></div>}></Layout>
+            <Layout head="Solicitud en vía pública" subheading="Datos del ciudadano" children={<Card></Card>}></Layout>
+            <Footer></Footer>
         </>
     );
 }
