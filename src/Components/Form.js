a5690c36182e995dd2f0a8f3df0d3245bfa3eeff
Malinali Becerril <mali@devf.mx>
2021-03-24 20:10:46 -0600

    add changes to forms

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,12 +1,46 @@
 import Button from "./Button";
+import useForm from "../hooks/useForm";
+import Input from "./Input";
+export default function Form({submit, inputsData, textBtn, children}) {
 
-export default function Form({submit, textBtn, children}) {
+    const {
+        inputs,
+        handleSubmit,
+        handleInputs,
+    } = useForm(submit,
+        {}
+    );
+
+    const selectType = (i, n) => {
+        switch(i.inputType){
+            case 'select':
+                return <></>
+            case 'photos':
+                return <></>
+            default:
+                return <Input
+                        label={i.label}
+                        type={i.type}
+                        key={i.name}
+                        placeholder={i.placeholder}
+                        change={handleInputs}
+                        value={inputs[i.value]}
+                        required={i.required}
+                        name={i.name}
+                    />
+        }
+    }
     return(
-    <form onSubmit={submit}className="container">
-        <div className="d-flex flex-wrap justify-content-center">
-            {children}
-        </div>
-        <Button type="submit" text={textBtn}></Button>
-    </form>
+        <form onSubmit={handleSubmit} className="container">
+            <div className="d-flex flex-wrap justify-content-center">
+                {inputsData ? 
+                    inputsData.map((i,index) => 
+                    selectType(i,index))
+                    : ''
+                }
+                {children}
+            </div>
+            <Button type="submit" text={textBtn}></Button>
+        </form>
     );
 };
