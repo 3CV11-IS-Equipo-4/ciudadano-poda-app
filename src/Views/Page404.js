d825c84eb28de948bc983102b5d6c504e03536db
Malinali Becerril <mali@devf.mx>
2021-03-25 09:21:31 -0600

    add flow forms

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import Navbar from '../Components/Navbar';
+import Layout from '../Components/Layout';
+
+
 export default function Page404() {
     return(
-        <>
-            <Navbar type={"ciudadano"}></Navbar>
-
+        <Layout head="No encontrado :c" subheading="404">
             <h1>Esa ruta no existe</h1>
             <Link to="/">Volver al inicio</Link>
-        </>
+        </Layout>
     );
 }
