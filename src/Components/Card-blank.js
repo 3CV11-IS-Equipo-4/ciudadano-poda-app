08a0588623e4d862fbbdb6b9c8f625a9db9c9d83
Malinali Becerril <mali@devf.mx>
2021-03-25 11:28:15 -0600

    changes profile

--- src/Components/Card-blank.js
+++ src/Components/Card-blank.js
@@ -1,11 +1,11 @@
 import './Card.css';
 
-export default function Card({title,content}) {
+export default function Card({title, children}) {
     return(
         <div className="card d-flex justify-content-center">
             <div className="card-body d-flex justify-content-center">
                 <h5 className="card-title ">{title}</h5>
-                {content}
+                <div>{children}</div>
             </div>
         </div>
     );
